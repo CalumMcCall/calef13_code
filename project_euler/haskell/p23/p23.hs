07475bd3e23e0935cf74d81532910f9485e7111b
calef13 <calef13@gmail.com>
2011-07-07 19:01:12 +0100

    Added global var for search limit. Added main function

--- project_euler/haskell/p23/p23.hs
+++ project_euler/haskell/p23/p23.hs
@@ -1,3 +1,7 @@
+--All numbers after max can be expressed as sum of two abundant numbers
+--so no point in checking higher
+let max = 28123
+
 divisors :: Integer -> [Integer]
 divisors x = [y | y <- [1..div x 2], rem x y == 0]
 
@@ -6,3 +10,8 @@ isAbundant x = (sum $ divisors x) > x
 
 getAbundantNums :: Integer -> [Integer]
 getAbundantNums x = filter isAbundant [1..x]
+
+main = do   let abNums = getAbundantNums max
+            putStrLn "Generated required abundant numbers"
+            noAbNums = [x | x <- [1..max], elem x abNums]
+
